db83e0d8deeac79dd5cbbfbf248b7c8a6fd5a8b0
Felix Yang <felixyang2028@u.northwestern.edu>
2025-04-09 14:00:35 -0500

    alu testbench remaining

--- src/alu/alu.v
+++ src/alu/alu.v
@@ -7,29 +7,29 @@
 // 0001     :   ADD
 // 0101     :   AND
 // 1001     :   NOT
-//
-// TODO: nzp, write bus to reg file
+// verilog_format: off
+// TODO: testbench
 
 module alu (
+    input clk,
     input   [15:0]  a, b,
     input   [ 3:0]  opcode,
     output  [15:0]  out,
-    output  [ 3:0]  nzp
+    output  [ 2:0]  nzp
 );
     reg [15:0] alu_out;
     assign out = alu_out;
+    assign nzp[1] = !(^out); // unary xnor
+    assign nzp[2] = out[15]; // MSB
+    assign nzp[0] = !out[15];
 
-    always @(*) begin
-        case (op)
+    always @(posedge clk)
+    begin
+        case (opcode)
             4'b0001 :   alu_out = a + b;
             4'b0101 :   alu_out = a & b;
             4'b1001 :   alu_out = ~a;
-            default :   0; // ?
+            default :   alu_out = 16'b0; // should create error code
         endcase
     end
 endmodule
-
-// ALU output bus
-module alu_bus ();
-endmodule
-
